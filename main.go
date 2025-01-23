00a120d857f413741137c7ac2e26abe5fe9b716f
nitishsharma2825 <sharnit2825@gmail.com>
2025-01-23 23:34:48 +0530

    Relational Tables on top of KV

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"internal/syscall/unix"
 	"log"
@@ -788,4 +789,164 @@ func (db *KV) pageReadFile(ptr uint64) []byte {
 		start = end
 	}
 	panic("bad ptr")
+}
+
+const (
+	TYPE_BYTES = 1 // string (of arbitrary bytes)
+	TYPE_INT64 = 2 // integer; 64 bit signed
+)
+
+// table cell
+type Value struct {
+	Type uint32 // tagged union
+	I64  int64
+	Str  []byte
+}
+
+// table row
+type Record struct {
+	Cols []string
+	Vals []Value
+}
+
+func (rec *Record) AddStr(col string, val []byte) *Record {
+	rec.Cols = append(rec.Cols, col)
+	rec.Vals = append(rec.Vals, Value{Type: TYPE_BYTES, Str: val})
+	return rec
+}
+
+func (rec *Record) AddInt64(col string, val int64) *Record {
+	rec.Cols = append(rec.Cols, col)
+	rec.Vals = append(rec.Vals, Value{Type: TYPE_INT64, I64: val})
+	return rec
+}
+func (rec *Record) Get(col string) *Value {
+	idxVal := 0
+	for idx, val := range rec.Cols {
+		if val == col {
+			idxVal = idx
+			break
+		}
+	}
+	return &rec.Vals[idxVal]
+}
+
+type TableDef struct {
+	// user defined
+	Name  string
+	Types []uint32 // col types
+	Cols  []string // col names
+	PKeys int      // the first `PKeys` columns are the primary key
+	// auto-assigned B-tree key prefixes for different tables
+	Prefix uint32
+}
+
+var TDEF_TABLE = &TableDef{
+	Prefix: 2,
+	Name:   "@table",
+	Types:  []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:   []string{"name", "def"},
+	PKeys:  1,
+}
+
+var TDEF_META = &TableDef{
+	Prefix: 1,
+	Name:   "@meta",
+	Types:  []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:   []string{"key", "val"},
+	PKeys:  1,
+}
+
+// get a single row by the PK
+func (db *DB) Get(table string, rec *Record) (bool, error) {
+	tdef := getTableDef(db, table)
+	if tdef == nil {
+		return false, fmt.Errorf("table not found: %s", table)
+	}
+	return dbGet(db, tdef, rec)
+}
+
+func (db *DB) Insert(tablestring, recRecord) (bool, error)
+func (db *DB) Update(tablestring, recRecord) (bool, error)
+func (db *DB) Upsert(tablestring, recRecord) (bool, error)
+func (db *DB) Delete(tablestring, recRecord) (bool, error)
+
+type DB struct {
+	Path string
+	kv   KV
+}
+
+// get a single row by the primary key
+func dbGet(db *DB, tdef *TableDef, rec *Record) (bool, error) {
+	// 1. reorder the input cols according to the schema
+	values, err := checkRecord(tdef, *rec, tdef.PKeys)
+	if err != nil {
+		return false, err
+	}
+	// 2. encode the primary key
+	key := encodeKey(nil, tdef.Prefix, values[:tdef.PKeys])
+	// 3. query the KV store
+	val, ok := db.kv.Get(key)
+	if !ok {
+		return false, nil
+	}
+	// 4. decode the value into columns
+	for i := tdef.PKeys; i < len(tdef.Cols); i++ {
+		values[i].Type = tdef.Types[i]
+	}
+	decodeValues(val, values[tdef.PKeys:])
+	rec.Cols = tdef.Cols
+	rec.Vals = values
+	return true, nil
+}
+
+// reorder a record and check for missing cols
+// n == tdef.Pkeys: record is exactly a PK
+// n == len(tdef.Cols): record contains all cols
+func checkRecord(tdef *TableDef, rec Record, n int) ([]Value, error)
+
+// encode cols for the "key" of the KV
+func encodeKey(out []byte, prefix uint32, vals []Value) []byte
+
+// decode columns from the "value" of the KV
+func decodeValues(in []byte, out []Value)
+
+func getTableDef(db *DB, name string) *TableDef {
+	rec := (&Record{}).AddStr("name", []byte(name))
+	ok, err := dbGet(db, TDEF_TABLE, rec)
+	if !ok {
+		return nil
+	}
+
+	tdef := &TableDef{}
+	err = json.Unmarshal(rec.Get("def").Str, tdef)
+	return tdef
+}
+
+const (
+	MODE_UPSERT      = 0 // insert or replace
+	MODE_UPDATE_ONLY = 1 // update existing keys
+	MODE_INSERT_ONLY = 2 // only add new keys
+)
+
+type UpdateReq struct {
+	tree *Btree
+	// out
+	Added bool // added a new key
+	// in
+	Key  []byte
+	Val  []byte
+	Mode int
+}
+
+func (tree *Btree) Update(req *UpdateReq)
+
+func dbUpdate(db *DB, tdef *TableDef, rec Record, mode int) (bool, error) {
+	values, err := checkRecord(tdef, rec, len(tdef.Cols))
+	if err != nil {
+		return false, err
+	}
+	key := encodeKey(nil, tdef.Prefix, values[:tdef.PKeys])
+	val := encodeValues(nil, values[tdef.PKeys:])
+	return db.kv.Update(key, val, mode)
 }
